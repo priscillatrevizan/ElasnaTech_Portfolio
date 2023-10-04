f6ef856b2dfd8d2c13149b7679b2d509f6520710
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2023-10-04 00:43:36 -0300

    feat: dark mode

--- toggle.js
+++ toggle.js
@@ -1,19 +1,28 @@
-const btnMobile = document.getElementById('btn-mobile');
+const changeThemeBtn = document.querySelector("#change-theme");
 
-function toggleMenu (event){
-    if(event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
-    const active = nav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expended', active);
-
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
-    }else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
-    };
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
 }
 
+// Load light or dark mode
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark");
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+  if (darkMode) {
+    toggleDarkMode();
+  }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+  toggleDarkMode();
+
+  // Save or remove dark mode from localStorage
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
+});
